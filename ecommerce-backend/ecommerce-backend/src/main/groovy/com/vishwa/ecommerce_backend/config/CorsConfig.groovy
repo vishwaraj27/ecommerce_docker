57fcd8b64f96d9aef191e4e4b647d5aeeecae186
vishwaraj27 <vishwasannasi@gmail.com>
2025-12-07 19:07:04 +0530

    cors change

--- ecommerce-backend/ecommerce-backend/src/main/groovy/com/vishwa/ecommerce_backend/config/CorsConfig.groovy
+++ ecommerce-backend/ecommerce-backend/src/main/groovy/com/vishwa/ecommerce_backend/config/CorsConfig.groovy
@@ -23,6 +23,10 @@ class CorsConfig {
         // 🌍 YOUR EC2 FRONTEND (Elastic IP)
         config.addAllowedOrigin("http://3.108.100.219")
 
+          // 🌍 Production domain (IMPORTANT: scheme must match exactly)
+        config.addAllowedOrigin("https://vancart.in")
+        config.addAllowedOrigin("https://www.vancart.in")  // if you ever use www
+
         // Allow all headers and methods
         config.addAllowedHeader("*")
         config.addAllowedMethod("*")
